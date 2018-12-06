ba5d9b819d581430a37e0559fb53ccb4f8873810
Lukács Bence <lukacsb@gmail.com>
2018-12-06 08:14:37 +0100

    refactored security

--- src/main/java/com/codecool/notificationhelper/security/WebSecurityConfiguration.java
+++ src/main/java/com/codecool/notificationhelper/security/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableOAuth2Sso
@@ -11,13 +12,18 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
                 .antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/", "/index", "/login**", "/webjars/**", "/error**")
-                .permitAll()
+                    .authorizeRequests()
+                    .antMatchers("/", "/index", "/login**", "/webjars/**", "/error**")
+                    .permitAll()
                 .anyRequest()
-                .authenticated();
+                    .authenticated()
+                    .and()
+                .logout()
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+                    .and()
+                .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
