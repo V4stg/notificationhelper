44e994746c7beca3c19c498475c1dc5b25d47121
Lukács Bence <lukacsb@gmail.com>
2019-01-03 11:21:02 +0100

    refactored request sending script on logout button; refactored navbar

--- src/main/java/com/codecool/notificationhelper/security/WebSecurityConfiguration.java
+++ src/main/java/com/codecool/notificationhelper/security/WebSecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableOAuth2Sso
@@ -23,7 +22,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .authenticated()
                     .and()
                 .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .invalidateHttpSession(true)
                     .clearAuthentication(true)
                     .logoutSuccessUrl("/")
@@ -31,7 +29,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .csrf()
-//                    .ignoringAntMatchers("/login")
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         ;
     }
